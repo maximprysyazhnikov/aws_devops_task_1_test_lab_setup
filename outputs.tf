003534e856672f2abe75069dc34fcb89849e1c6d
Присяжніков <150026110+maximprysyazhnikov@users.noreply.github.com>
2025-08-27 16:58:44 +0300

    Solution

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "vpc_id" {
-    description = "ID of the VPC, deployed by the module"
-    value       = aws_vpc.network.id
+  description = "ID of the VPC, deployed by the module"
+  value       = aws_vpc.main.id
+}
+
+output "vpc_cidr" {
+  description = "CIDR of the VPC"
+  value       = aws_vpc.main.cidr_block
 }
